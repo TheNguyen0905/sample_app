b99ee828737d88132b36faa3ef7d40e710bc7e78
The Nguyen <thebay0905@gmail.com>
2018-07-31 11:07:59 +0700

    Add password reset

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,23 +1,30 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = {:host => "localhost:3000"}
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_caching = false
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 
   config.cache_classes = false
   config.eager_load = false
   config.consider_all_requests_local = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
-  host = "localhost:3000" # Don"t use this literally; use your local dev host instead
-  config.action_mailer.default_url_options = { host: host, protocol: "https" }
 
   if Rails.root.join("tmp", "caching-dev.txt").exist?
     config.action_controller.perform_caching = true
-
     config.cache_store = :memory_store
     config.public_file_server.headers = {
       "Cache-Control" => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
-
     config.cache_store = :null_store
   end
 
